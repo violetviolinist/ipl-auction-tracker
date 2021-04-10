34a49c9fca883159962573c74b4b75338f256bf9
Jay Parekh <jay@MacBook-Pro.local>
2021-04-10 21:48:43 +0530

    add mongo

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+const dbConnect = require("./db/dbConnect");
+
+// execute database connection 
+dbConnect();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
