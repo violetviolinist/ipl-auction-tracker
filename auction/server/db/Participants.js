15dedb2514751054ba4a0d428fec098e5c15fd3e
Jay Parekh <jay@MacBook-Pro.local>
2021-04-11 17:25:25 +0530

    folder restructuring

--- auction/server/db/Participants.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const ParticipantSchema = new mongoose.Schema({}, {strict: false});
-
-module.exports = mongoose.model("Participants", ParticipantSchema);
--- auction/server/db/Players.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const PlayerSchema = new mongoose.Schema({}, {strict: false});
-
-module.exports = mongoose.model("Players", PlayerSchema);
